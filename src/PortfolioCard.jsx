dc599841a405df6e531232c686381653e26261bf
nathan-gissler <112874234+nathan-gissler@users.noreply.github.com>
2024-05-02 12:21:00 +0200

    feat: modify some css on portfolio

--- src/PortfolioCard.jsx
+++ src/PortfolioCard.jsx
@@ -5,8 +5,10 @@ export default function PortfolioCard({ musicStyleId, index, content }) {
     return (
         <div id={camelCaseToKebabCase(musicStyleId)} className="portfolio">
             <div className="half-portfolio">
-                <div className="portfolio-card-title">{content.musicStyleName}</div>
-                <div className="portfolio-card-description">{content.musicStyleDescription}</div>
+                <div className="portfolio-card-title-description">
+                    <div className="portfolio-title">{content.musicStyleName}</div>
+                    <div className="portfolio-description">{content.musicStyleDescription}</div>
+                </div>
             </div>
             <div className="half-portfolio">
                 <AudioPlayer musicStyleId={musicStyleId} index={index} />
