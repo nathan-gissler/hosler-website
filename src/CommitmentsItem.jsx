1c9f2ee2cf3275ca9cef06e45578dad6c09d5d0e
nathan-gissler <112874234+nathan-gissler@users.noreply.github.com>
2024-07-16 17:00:28 +0200

    feat: change commitments layout

--- src/CommitmentsItem.jsx
+++ src/CommitmentsItem.jsx
@@ -1,27 +1,18 @@
 export default function CommitmentsItem({ invertedLayout, content }) {
     let itemText = (
-        <div className="commitment-text">
+        <div className={`commitment-text ${invertedLayout ? "align-right" : "align-left"}`}>
             <div className="commitment-title">{content.title}</div>
             <div className="commitment-description">{content.description}</div>
         </div>
     )
     let itemImage = (
-        <div className="commitment-image" />
+        <div className={`commitment-image ${invertedLayout ? "image-gradient-right-left" : "image-gradient-left-right"}`} />
     )
 
-    if (invertedLayout) {
-        return (
-            <div className="commitment">
-                {itemImage}
-                {itemText}
-            </div>
-        )
-    } else {
-        return (
-            <div className="commitment">
-                {itemText}
-                {itemImage}
-            </div>
-        )
-    }
+    return (
+        <div className="commitment">
+            {itemImage}
+            {itemText}
+        </div>
+    )
 }
